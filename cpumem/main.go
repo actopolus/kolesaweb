c1da1be84ce6f4b830a31c41bffa33fe3a09f20f
activist <bereznev@kolesa.kz>
2018-04-14 11:29:04 +0600

    Use tags for versions

--- cpumem/main.go
+++ cpumem/main.go
@@ -29,30 +29,3 @@ func findUser(in []string) (out []string) {
 
 	return out
 }
-
-// Найти пользователя версия 2
-func findUser2(in []string) (out []string) {
-	reg, _ := regexp.Compile("(?i)kolesaweb")
-
-	for _, user := range in {
-		if match := reg.MatchString(user); match {
-			out = append(out, user)
-		}
-	}
-
-	return out
-}
-
-// Найти пользователя версия 3
-func findUser3(in []string) ([]string) {
-	out    := make([]string, 0, len(in))
-	reg, _ := regexp.Compile("(?i)kolesaweb")
-
-	for _, user := range in {
-		if match := reg.MatchString(user); match {
-			out = append(out, user)
-		}
-	}
-
-	return out
-}
